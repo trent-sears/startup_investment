6060915791dc4e43287242caad999c475a257a53
Trent Sears <trent.sears.3030@gmail.com>
2020-05-12 19:39:30 -0600

    Need to build model, consider featurizing on type of investment

--- src/investment.py
+++ src/investment.py
@@ -5,7 +5,19 @@ import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 17})
 import folium
 
+from sklearn import preprocessing
+from sklearn.datasets import make_classification
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold, train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
 def clean(file_path):
+    '''
+    Parameters
+        file_path: takes in a file path
+    Returns
+        a cleaned df
+    '''
     df = pd.read_csv(file_path,encoding='latin1')
     df.dropna(inplace=True)
     df.drop(['permalink','region','founded_month','founded_quarter'],axis=1,inplace=True)
@@ -14,24 +26,63 @@ def clean(file_path):
     df['last_funding_at']= pd.to_datetime(df['first_funding_at'],errors='coerce')
     df['founded_year'] = df['founded_year'].astype('int64')
     df.drop(df[df['country_code']=='CAN'].index,inplace=True)
+    df['funding_total_usd'] = df[' funding_total_usd '].apply(lambda x: x.replace(' ','')).apply(lambda x: x.replace(',',''))
+    df['funding_total_usd'] = df['funding_total_usd'].apply(lambda x: x.replace('-','0'))
+    df['funding_total_usd'] = df['funding_total_usd'].astype('int64')
+    df['market'] = df[' market '].apply(lambda x: x.replace(' ',''))
+    df.drop(' market ',axis=1,inplace=True)
+    df.drop(' funding_total_usd ',axis=1,inplace=True)
+    df.drop('country_code',axis=1,inplace=True)
+    df.drop('homepage_url',axis=1,inplace=True)
+    df.drop('name',axis=1,inplace=True)
+#     df['status'] = df['status'].apply(lambda x: x.replace('operating','0')).apply(lambda x: x.replace('acquired','0')).apply(lambda x: x.replace('closed','1'))
+#     df['status'] = df['status'].astype('int64')
     return df
 
 def feature_engineer(df):
-    df['time_to_funding'] = df['first_funding_at']-df['founded_at']
+    '''
+    Parameters
+        df: Takes in a pandas data frame
+    Returns
+        a data frame with engineered features
+    '''
+    df['time_to_funding'] = abs((df['first_funding_at']-df['founded_at']).dt.days)
+    df.dropna(inplace=True)
     return df
 
+def create_pie_charts(df,column,column_val,target):
+      '''
+    Parameters
+        df: Cleaned data frame
+        column: column of data frame used to split data as string
+        column_val: Value we are looking for in column as string
+        target: The target values we are trying to predict
+    Returns
+        a saved image in the images folder
+    '''
+    column_val_title = ' '.join(re.findall('[A-Z][^A-Z]*',column_val))
+    pie_df = df[df[column]==column_val][target].value_counts().rename_axis(target).reset_index(name='counts')
+    pie_df['pct'] = pie_df['counts']/len(pie_df)
+    labels=pie_df[target]
+    fig, ax = plt.subplots(figsize=(14,7))
+    ax.pie(pie_df['pct'], explode=[0,0,.15], labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=50)
+    ax.axis('equal')
+    ax.set_title(f'{target.capitalize()} Of {column_val_title} Market')
+    plt.savefig(f'../images/{column_val}_pie.png',dpi=500)
 
-if name == '__main__':
-    df =clean('../../../Downloads/investments_VC.csv')
+
+if __name__ == '__main__':
+    cleaned_df =clean('../../../Downloads/investments_VC.csv')
 
     #plot 1
-    time_df = df['founded_year'].value_counts().rename_axis('year').reset_index(name='counts')
+    time_df = cleaned_df['founded_year'].value_counts().rename_axis('year').reset_index(name='counts')
     time_df.sort_values(by='year',inplace=True)
     time_df
     x1 = time_df[time_df['year']>1980]['year']
     y1 = time_df[time_df['year']>1980]['counts']
 
-    funding_df = df['first_funding_at'].dt.year.value_counts().rename_axis('year').reset_index(name='counts')
+    funding_df = cleaned_df['first_funding_at'].dt.year.value_counts().rename_axis('year').reset_index(name='counts')
     funding_df['year'] = funding_df['year'].astype('int64')
     funding_df.sort_values(by='year',inplace=True)
     x2 = funding_df[funding_df['year']>1980]['year']
@@ -48,7 +99,7 @@ if name == '__main__':
     plt.xticks(rotation=45,ha='center')
 
     #plot 2
-    state_df = df['state_code'].value_counts().rename_axis('state').reset_index(name='counts')
+    state_df = cleaned_df['state_code'].value_counts().rename_axis('state').reset_index(name='counts')
     state_df.sort_values(by='state',inplace=True)
     url = 'https://raw.githubusercontent.com/python-visualization/folium/master/examples/data'
     state_geo = f'{url}/us-states.json'
@@ -84,7 +135,7 @@ if name == '__main__':
     m
 
     #Plot three
-    pie_df= df['status'].value_counts().rename_axis('status').reset_index(name='counts')
+    pie_df= cleaned_df['status'].value_counts().rename_axis('status').reset_index(name='counts')
     pie_df['pct'] = pie_df['counts']/len(df)
     labels=pie_df['status']
     fig, ax = plt.subplots(figsize=(14,7))
